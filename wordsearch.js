f4d288fa8509471b34f1a9f9a68fceda07f8ac1b
vagrant <b-a-clark@shaw.cagit config --global user.name Brett>
2021-07-29 00:14:35 +0000

    Pair programming, word search completed

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map((ls) => ls.join(""));
+    let verticalJoin = [];
+    let verticalWord = [];
+  
+    if (letters[0] !== undefined) {
+      for (let i = 0; i < letters[0].length; i++) {
+        for (let j = 0; j < letters.length; j++) {
+          verticalWord.push(letters[j][i]);
+        }
+        verticalJoin[i] = verticalWord.join("");
+        verticalWord = [];
+      }
+  
+      for (l of horizontalJoin) {
+        if (l.includes(word)) return true;
+      }
+  
+      for (l of verticalJoin) {
+        if (l.includes(word)) return true;
+      }
     }
-}
-
-module.exports = wordSearch
+    if (letters.length === 0) {
+      return false;
+    }
+  
+    return false;
+  };
+  
+  module.exports = wordSearch;
